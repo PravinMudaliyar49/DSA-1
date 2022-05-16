db58fa3d75d743ff22bc18f16d9c0eb80af49562
Pravin <pravincoding49@gmail.com>
2022-05-16 20:25:22 +0530

    16th May commit

---
+++ 16th May/basics.java
@@ -0,0 +1,30 @@
+public class basics{
+    public static void main(String[] args) {
+        System.out.println(188327498);
+
+        int x = 180101;
+        
+        System.out.println(x);
+
+        String y = "I went to a place on 21st April";
+        
+        System.out.println(y);
+
+        char z = '@';
+        
+        System.out.println(z);
+
+        double a = 10.2345;
+        
+        boolean b = true;
+        boolean c = false;
+        
+        long d = 21345687;
+
+        int mukeshKaAge = 18;
+        
+        System.out.println(mukeshKaAge);
+
+
+    }
+}
