0cddcec9ef2e1e3813be95101d050bd037dee904
Pravin <pravincoding49@gmail.com>
2022-05-19 20:25:42 +0530

    19th May commit

---
+++ 19th May/bodmas.java
@@ -0,0 +1,12 @@
+public class bodmas {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        int c = 2;
+        int d = 9;
+
+        int ans = a + (b - c) * d;
+
+        System.out.println(ans);
+    }
+}
---
+++ 19th May/salaryCalc.java
@@ -0,0 +1,21 @@
+import java.nio.file.attribute.FileTime;
+import java.util.*;
+
+public class salaryCalc {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int salary = sc.nextInt();
+        int increment = sc.nextInt();
+        int year = sc.nextInt();
+
+        int difference = year - 2022;
+        int incRupees = salary * increment / 100;
+
+        int rupee = difference * incRupees;
+
+        int futureSalary = rupee + salary;
+
+        System.out.println("Your future salary is " + futureSalary + ".");
+
+    }
+}
