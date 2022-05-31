209587f70496643529ea95ae5e4aaeba60dc8567
Pravin <pravincoding49@gmail.com>
2022-05-31 20:24:57 +0530

    31st May

---
+++ 31st May/loopingStatements.java
@@ -0,0 +1,21 @@
+// package 31st May;
+
+public class loopingStatements {
+    public static void main(String[] args) {
+        // System.out.println("Hello world");
+        // System.out.println("Hello world");
+        // System.out.println("Hello world");
+        // System.out.println("Hello world");
+        // System.out.println("Hello world");
+        // System.out.println("Hello world");
+        // System.out.println("Hello world");
+        // System.out.println("Hello world");
+        // System.out.println("Hello world");
+        // System.out.println("Hello world");
+
+        for (int i = -10; i <= 3; i = i + 1) {
+            System.out.println(i);
+        }
+
+    }
+}
