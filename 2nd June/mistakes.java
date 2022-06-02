0b993f5c14c2d2747a638d8594004839c41a8a70
Pravin <pravincoding49@gmail.com>
2022-06-02 20:21:50 +0530

    2nd June

---
+++ 2nd June/mistakes.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class mistakes {
+    public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
+
+        for (int i = 1; i <= 10; i--) {
+            System.out.println(i);
+        }
+
+        for(int i = 10; i > 5; i++){
+            System.out.println(i);
+        }
+
+        // int i = 10;
+        
+        // for(; i < 15; i++){
+        //     System.out.println(i);
+        // }
+
+        for(int i = 10; i < 15; ){
+            System.out.println(i);
+        }
+        
+        int n1 = 10;
+        int n2 = 20;
+
+        for(int i = 10; n1 <= n2; i++){
+            System.out.println(i);
+        }
+        
+        for(int i = 10; ; i++){
+            System.out.println(i);
+        }
+
+
+
+    }
+}
